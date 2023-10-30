8aa00dde6b75ea67a3d3c48a1282884412857597
kimusamu <57126265+kimusamu@users.noreply.github.com>
2023-10-30 10:42:56 +0900

    make game_world
    make game_world

---
+++ game_world.py
@@ -0,0 +1,21 @@
+world = [[], [], []]
+
+def add_object(o, depth = 0):
+    world[depth].append(o)
+
+def remove_object(o):
+    for layer in world:
+        if o in layer:
+            layer.remove(o)
+            return
+    raise ValueError('Cannot delete non existing object')
+
+def update():
+    for layer in world:
+        for o in layer:
+            o.update()
+
+def render():
+    for layer in world:
+        for o in layer:
+            o.draw()
