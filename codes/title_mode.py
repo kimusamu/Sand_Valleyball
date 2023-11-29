3a900270ec4d3907087cbfabd25ce672215f660e
Kim Sang Yun <grinder0617@gmail.com>
2023-11-30 03:47:37 +0900

    add tutorial_mode
    add tutorial_mode

--- codes/title_mode.py
+++ codes/title_mode.py
@@ -1,10 +1,10 @@
 import game_framework
-from pico2d import load_image, delay, clear_canvas, update_canvas, get_events, get_time, SDL_QUIT, SDL_KEYDOWN, SDLK_ESCAPE, SDLK_SPACE, SDLK_a, SDLK_e
-from codes import play_mode, play_with_mode
+from pico2d import load_image, delay, clear_canvas, update_canvas, get_events, get_time, SDL_QUIT, SDL_KEYDOWN, SDLK_ESCAPE, SDLK_SPACE, SDLK_a, SDLK_e, SDLK_t
+from codes import play_mode, play_with_mode, tutorial_mode
 
 def init():
     global image
-    image = load_image('title.png')
+    image = load_image('titles.png')
 
 def finish():
     global image
@@ -21,6 +21,8 @@ def handle_events():
             game_framework.change_mode(play_mode)
         elif event.type == SDL_KEYDOWN and event.key == SDLK_e:
             game_framework.change_mode(play_with_mode)
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_t:
+            game_framework.change_mode(tutorial_mode)
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             game_framework.quit()
 
